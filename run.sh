a593479859c075c1e0408996adbe52651c142010
pyyaoer <pyyaoer@gmail.com>
2023-02-01 02:44:34 +0800

    Add leading zeros for iodepth in output filenames

--- run.sh
+++ run.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-debug="no"
+#debug="no"
 
-device="md0"
-rw="read"
-bs="1M"
-iodepth=16
-runtime=30
+device="md1"
 
 init () {
-	device="md0"
 	rw="read"
 	bs="1M"
 	iodepth=16
 	if [[ -z $debug ]]; then
-		runtime=30
+		runtime=3
 	else
 		runtime=900
 	fi
 }
 
+run_fio() {
+	long_iodepth=$(printf "%03d" $iodepth)
+	filename="$rw-$device-$bs-$long_iodepth-$runtime.txt"
+	echo $filename
+	rw=$rw device=$device runtime=$runtime bs=$bs iodepth=$iodepth fio config.fio > $filename
+}
+
 iodepth_test () {
 	init
 	iodepth=1
-	for i in {1..5}; do
-		filename="$rw-$device-$bs-$iodepth-$runtime.txt"
-		echo $filename
-		rw=$rw device=$device runtime=$runtime bs=$bs iodepth=$iodepth fio config.fio > $filename
+	for i in {1..10}; do
+		run_fio
 		let iodepth\*=2
 	done
 }
@@ -34,10 +34,11 @@ iodepth_test () {
 rw_test () {
 	init
 	rw_pool=('write' 'randread' 'randwrite' 'rw')
-	for rw in "${rw_pool[@]}"; do
-		filename="$rw-$device-$bs-$iodepth-$runtime.txt"
-		echo $filename
-		rw=$rw device=$device runtime=$runtime bs=$bs iodepth=$iodepth fio config.fio > $filename
+	for i in {1..4}; do
+		for rw in "${rw_pool[@]}"; do
+			run_fio
+		done
+		let iodepth\*=2
 	done
 }
 
